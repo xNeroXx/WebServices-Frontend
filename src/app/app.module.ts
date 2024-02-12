e7586783eeea2c217bddacef0cc3b2210d16e4e4
Leon Kleinhans <leon_kl.03@gmx.de>
2024-02-12 17:55:17 +0100

    minimal changes in import statements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {JwtInterceptor} from "./classes/JwtInterceptor";
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import {LoginFormComponent} from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
