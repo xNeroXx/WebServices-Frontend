2d374f521eaa434153ac1caff23e77e285a6acee
xNeroXx <114223803+xNeroXx@users.noreply.github.com>
2024-02-12 16:11:56 +0100

    added dummy component

---
+++ src/app/components/dummy/dummy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy',
+  templateUrl: './dummy.component.html',
+  styleUrl: './dummy.component.scss'
+})
+export class DummyComponent {
+
+}
