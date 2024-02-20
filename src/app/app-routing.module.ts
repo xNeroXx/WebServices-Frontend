d3bfa2605331ab444b10d257f3260b338ae599cd
Leon Kleinhans <leon_kl.03@gmx.de>
2024-02-20 15:04:33 +0100

    added main-page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
+import {MainPageComponent} from "./components/main-page/main-page.component";
 import {LoginFormComponent} from "./components/login-form/login-form.component";
 import {DummyComponent} from "./components/dummy/dummy.component";
 
 const routes: Routes = [
 
   {
-    path: '', // standard url leads to home with navbar
-    component: HomeComponent,
-    children: [ //one of these routes will display home content additionally to router outlet
+    path: '', // standard url leads to main-page with navbar
+    component: MainPageComponent,
+    children: [ //one of these routes will display main-page content additionally to router outlet
       {
       path: 'dummy',
       component: DummyComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,13 @@ import {provideAnimationsAsync} from '@angular/platform-browser/animations/async
 import {HTTP_INTERCEPTORS, provideHttpClient, withFetch} from "@angular/common/http";
 import {JwtInterceptor} from "./classes/JwtInterceptor";
 import {LoginFormComponent} from './components/login-form/login-form.component';
-import {HomeComponent} from './components/home/home.component';
-import {DummyComponent} from './components/dummy/dummy.component';
+import {MainPageComponent} from "./components/main-page/main-page.component";
+import {DummyComponent} from "./components/dummy/dummy.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    MainPageComponent,
     DummyComponent
   ],
   imports: [
