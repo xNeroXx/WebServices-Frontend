c773ef46559486e05d296e47704c55eb39767f29
Leon Kleinhans <leon_kl.03@gmx.de>
2024-02-23 21:30:48 +0100

    adjusted upload button text to make it german and give more context

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -2,13 +2,14 @@ import {Component} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
+import {MatPrefix} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-upload',
   templateUrl: 'upload.component.html',
   styleUrls: ['upload.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatTooltipModule, MatIconModule],
+  imports: [MatButtonModule, MatTooltipModule, MatIconModule, MatPrefix],
 })
 export class UploadComponent {
 
