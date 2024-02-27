95f307bf756d52ae7249836f01ff3f5c8c367486
Leon Kleinhans <leon_kl.03@gmx.de>
2024-02-27 01:07:37 +0100

    added more styling and functioning logic to login component

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -2,8 +2,11 @@ import {Component} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
-import {LoginService} from "../../services/login.service";
-import {MatButton} from "@angular/material/button";
+import {UserService} from "../../services/user.service";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatDivider} from "@angular/material/divider";
+import {RouterLink} from "@angular/router";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-login-form',
@@ -11,28 +14,30 @@ import {MatButton} from "@angular/material/button";
   styleUrl: './login-form.component.scss',
   standalone: true,
   imports: [
-    MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButton
+    MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButton, MatDivider, RouterLink, MatIcon, MatIconButton
   ]
 })
 export class LoginFormComponent {
+  hide = true;
   loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
+    email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$/)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
   });
 
-  constructor(private loginService: LoginService) {}
-/*
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
+  constructor(private userService: UserService) {}
+
+  /*getErrorMessage() {
+    if (this.loginForm.get('email')?.hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-*/
+    return this.loginForm.get('email')?.hasError('email') ? 'Not a valid email' : '';
+  }*/
+
   submit() {
     console.log(this.loginForm.get('email')?.value)
     console.log(this.loginForm.get('password')?.value)
+    this.userService.login(this.loginForm.value.email ?? '', this.loginForm.value.password ?? '')
   }
 
 
