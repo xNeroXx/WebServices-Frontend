0b26b2a5b54a8f06d0962e122eaeea09ce9f96a3
Leon Kleinhans <leon_kl.03@gmx.de>
2024-02-28 17:53:27 +0100

    fixed _searchForSongs method in search service

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -48,7 +48,9 @@ export class SearchService {
   }
 
   private _searchForSongs(searchValue: string, searchCategory: string) {
-    this.http.post<SongData[]>(this.searchURL, {searchCategory, searchValue}).subscribe( //TODO research errorhandling thats not deprecated
+    let searchData: {[p: string]: string} = {};
+    searchData[searchCategory] = searchValue;
+    this.http.post<SongData[]>(this.searchURL, {searchData}).subscribe( //TODO research errorhandling thats not deprecated
       (data) => {
         this.searchResponse = data;
         this.router.navigate(['/searchResults']);
