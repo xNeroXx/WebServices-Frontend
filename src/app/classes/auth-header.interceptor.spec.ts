4e90939c7281ae8ba239721d420014fc9fc59836
Noura <noura.bouhra@gmail.com>
2024-02-28 22:34:17 +0100

    commit login auth

--- src/app/classes/auth-header.interceptor.spec.ts
+++ src/app/classes/auth-header.interceptor.spec.ts
@@ -4,7 +4,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { authHeaderInterceptor } from './auth-header.interceptor';
 
 describe('authHeaderInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
+  const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => authHeaderInterceptor(req, next));
 
   beforeEach(() => {
