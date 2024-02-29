e0e1ce707448470479ee812901b4ac7dbf4e9eeb
Leon Kleinhans <leon_kl.03@gmx.de>
2024-02-29 21:37:10 +0100

    created default values for song data

--- src/app/interfaces/song-data.ts
+++ src/app/interfaces/song-data.ts
@@ -7,3 +7,15 @@ export interface SongData {
   releaseYear: number,
   duration: number
 }
+
+export function createAutofillData(data: Partial<SongData> = {}): SongData {
+  return {
+    'id': data.id || 0,
+    'title': data.title || '',
+    'interpret': data.interpret || [],
+    'album': data.album || '',
+    'genre': data.genre || '',
+    'releaseYear': data.releaseYear || 0,
+    'duration': data.duration || 0
+  };
+}
