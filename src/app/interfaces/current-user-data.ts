6f31915922e5b3589183522a7309c7b160188807
Leon Kleinhans <leon_kl.03@gmx.de>
2024-03-01 17:53:27 +0100

    adjusted current user data interface and usages

--- src/app/interfaces/current-user-data.ts
+++ src/app/interfaces/current-user-data.ts
@@ -1,6 +1,15 @@
 export interface CurrentUserData {
+  id: number,
   email: string,
   first_name: string,
   last_name: string,
-  username: string
+  username: string,
+  address: {
+    street: string,
+    house_number: number,
+    postal_code: number,
+    city: string,
+    country: string,
+    state: string
+  }
 }
