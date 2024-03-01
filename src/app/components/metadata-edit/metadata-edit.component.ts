0d0b496e18ec3517b649cb2f812442e929478d95
Noura <noura.bouhra@gmail.com>
2024-03-01 11:12:54 +0100

    fixed logic to edit many artists names, added tooltip

--- src/app/components/metadata-edit/metadata-edit.component.ts
+++ src/app/components/metadata-edit/metadata-edit.component.ts
@@ -3,35 +3,34 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {MetadataService} from '../../services/metadata.service';
 
 @Component({
-    selector: 'app-metadata-edit',
-    templateUrl: './metadata-edit.component.html',
-    styleUrls: ['./metadata-edit.component.scss']
+  selector: 'app-metadata-edit',
+  templateUrl: './metadata-edit.component.html',
+  styleUrls: ['./metadata-edit.component.scss']
 })
 export class MetadataEditComponent {
-    selectedArtistName: string;
+  selectedArtistNames: string;
 
+  constructor(
+    public dialogRef: MatDialogRef<MetadataEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private metadataService: MetadataService
+  ) {
+    this.selectedArtistNames = data.song.artists.map((artist: { name: string; }) => artist.name).join('; ');
+  }
 
-    constructor(
-        public dialogRef: MatDialogRef<MetadataEditComponent>,
-        @Inject(MAT_DIALOG_DATA) public data: any,
-        private metadataService: MetadataService
-    ) {
-        this.selectedArtistName = data.song.artist[0].name;
-    }
+  cancel(): void {
+    this.dialogRef.close();
+  }
 
-    cancel(): void {
-        this.dialogRef.close();
-    }
+  save(): void {
+    const artistNames = this.selectedArtistNames.split(';').map(name => name.trim());
 
+    this.data.song.artists = artistNames.map(name => ({name: name}));
 
-    save(): void {
-        this.data.song.artist.name = this.selectedArtistName;
-        console.log(this.data.song);
-        this.metadataService.changeMetadata(this.data.song).subscribe(() => {
-            this.dialogRef.close();
-        }, (error: any) => {
-            console.error('Error updating metadata:', error);
-        });
-    }
-
+    this.metadataService.changeMetadata(this.data.song).subscribe(() => {
+      this.dialogRef.close();
+    }, (error: any) => {
+      console.error('Error updating metadata:', error);
+    });
+  }
 }
