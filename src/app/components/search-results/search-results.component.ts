6f3aa967128c175a81904f676b7fb8d9dd9c47f6
Leon Kleinhans <leon_kl.03@gmx.de>
2024-03-02 18:48:47 +0100

    changed search results component to match newer version of song card

--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SearchService} from "../../services/search.service";
 import {SongData} from "../../interfaces/song-data";
+import {SongService} from "../../services/song.service";
 
 @Component({
   selector: 'app-search-results',
@@ -9,25 +10,51 @@ import {SongData} from "../../interfaces/song-data";
 })
 export class SearchResultsComponent implements OnInit{
   protected searchResults: SongData[] = [];
-  protected selectedAudioSrc: string | null = null;
-  private isPlaying: boolean = false;
 
-  audioSrc = ''
+  songs: SongData[] = [];
+  selectedSong: SongData | null = null;
+  audioPlayer: HTMLAudioElement | null = null;
+  selectedSongId: number | null = null;
+  isPlaying = false;
 
 
-  constructor(private searchService: SearchService) {}
+
+  constructor(private searchService: SearchService, private songService: SongService) {}
 
   ngOnInit() {
     this.searchResults = this.searchService.searchResponse;
   }
 
-  playSong(audioSrc: string) {
-    if (this.selectedAudioSrc === audioSrc) {
-      this.isPlaying = !this.isPlaying;
-      this.selectedAudioSrc = null;
+  playSong(songId: number): void {
+    if (this.selectedSongId === songId && this.isPlaying) {
+      this.isPlaying = false;
+      if (this.audioPlayer) {
+        this.audioPlayer.pause();
+      }
+      this.selectedSongId = null;
     } else {
-      this.selectedAudioSrc = audioSrc;
-      this.isPlaying = true;
+      if (this.audioPlayer) {
+        this.audioPlayer.pause();
+      }
+      this.songService.getAudioSource(songId).subscribe(
+        (blob) => {
+          const audioUrl = URL.createObjectURL(blob);
+          this.audioPlayer = new Audio(audioUrl);
+          this.isPlaying = true;
+          this.selectedSongId = songId;
+          this.audioPlayer.play();
+        },
+        (error: any) => {
+          console.error('Fehler beim Abrufen der Audiodatei:', error);
+        }
+      );
+    }
+  }
+
+  pauseSong(): void {
+    if (this.audioPlayer) {
+      this.audioPlayer.pause();
+      this.isPlaying = false;
     }
   }
 
