72911f046e13e3d13d82c83eb12a042ed944cd0d
Leon Kleinhans <leon_kl.03@gmx.de>
2024-03-03 13:10:16 +0100

    added token refreshing on interval of 29 min

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from "rxjs";
+import {UserService} from "../../services/user.service";
+import {RefreshService} from "../../services/refresh.service";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit, OnDestroy{
+  private intervalRefresh?: Subscription;
+
+  constructor(private refreshService: RefreshService, private userService: UserService) {}
+
+  ngOnInit() {
+    this.intervalRefresh = interval(30 * 58 * 1000)
+      .subscribe(() => {
+        if(this.userService.getLoggedIn()){
+          console.log('Interval works')
+          this.refreshService.refreshToken();
+        }
+      })
+  }
+
+  ngOnDestroy() {
+    this.intervalRefresh?.unsubscribe();
+  }
 
 }
---
+++ src/app/services/refresh.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {TokenObject} from "../interfaces/token-object";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RefreshService {
+  private refreshURL = 'https://127.0.0.1:8000/api/registration/auth/refresh'
+
+  constructor(private http: HttpClient) {  }
+
+  refreshToken() {
+    return this.http.post<TokenObject>(this.refreshURL, {}).subscribe((data) => {
+      localStorage.clear();
+      localStorage.setItem("access_token", data.token.access_token);
+      localStorage.setItem("refresh_token", data.token.refresh_token);
+    })
+  }
+}
